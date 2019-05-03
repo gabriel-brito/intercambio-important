86901faabb9879a56ff9e377973a871e3daed34f
Marcos Sousa <devmarcosweb@gmail.com>
2019-05-03 20:50:21 -0300

    componente: resultado

--- src/components/routes.jsx
+++ src/components/routes.jsx
@@ -3,11 +3,13 @@ import { Router, Route, Redirect, hashHistory } from 'react-router';
 
 import Home from '../modules/home';
 import Checkout from '../modules/checkout';
+import Resultado from '../modules/resultado';
 
 export default () => (
   <Router history={hashHistory}>
     <Route path='/home' component={Home} />
     <Route path='/checkout' component={Checkout} />
+    <Route path='/resultado' component={Resultado} />
     <Redirect from='*' to='/home' />
   </Router>
 )
