e7b5fe3fb417e7ea35cc041584384b4059df946d
Marcos Sousa <devmarcosweb@gmail.com>
2019-05-04 00:48:11 -0300

    pagina: checkout

--- src/modules/checkout/formulario.js
+++ src/modules/checkout/formulario.js
@@ -1,9 +1,190 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+import Typography from '@material-ui/core/Typography';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 
 const Formulario = () => (
-  <section className='formulario'>
-    Formulário
-  </section>
+	<div>
+		<Grid container spacing={24}>
+			<Grid item xs={12}>
+				<React.Fragment>
+					<Typography variant="h6" gutterBottom>
+						Dados do Cliente
+					</Typography>
+					<Grid container spacing={24}>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="cpf"
+								name="cpf"
+								label="CPF"
+								fullWidth
+								autoComplete="fname"
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="email"
+								name="email"
+								label="E-mail"
+								fullWidth
+								autoComplete="fname"
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="firstName"
+								name="firstName"
+								label="Nome"
+								fullWidth
+								autoComplete="fname"
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="lastName"
+								name="lastName"
+								label="Sobrenome"
+								fullWidth
+								autoComplete="lname"
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<TextField
+								required
+								id="address1"
+								name="address1"
+								label="Endereço"
+								fullWidth
+								autoComplete="billing address-line1"
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="city"
+								name="city"
+								label="Cidade"
+								fullWidth
+								autoComplete="billing address-level2"
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								id="state"
+								name="state"
+								label="State/Province/Region"
+								variant="outlined"
+								fullWidth />
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="zip"
+								name="zip"
+								label="Cep"
+								fullWidth
+								autoComplete="billing postal-code"
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item xs={12} sm={6}>
+							<TextField
+								required
+								id="country"
+								name="country"
+								label="País"
+								fullWidth
+								autoComplete="billing country"
+								variant="outlined"
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<FormControlLabel
+								control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
+								label="Use este endereço para informações de pagamento"
+							/>
+						</Grid>
+					</Grid>
+				</React.Fragment>
+			</Grid>
+
+			<Grid item xs={12}>
+				<React.Fragment>
+					<Typography variant="h6" gutterBottom>
+						Informações de Pagamento
+				</Typography>
+					<Grid container spacing={24}>
+						<Grid item xs={12} md={6}>
+							<TextField 
+								required 
+								id="Bandeira" 
+								label="Bandeira" 
+								variant="outlined"
+								fullWidth />
+						</Grid>
+						<Grid item xs={12} md={6}>
+							<TextField 
+								required 
+								id="Número do Cartão" 
+								label="Card number" 
+								variant="outlined"
+								fullWidth />
+						</Grid>
+						<Grid item xs={12} md={6}>
+							<TextField 
+								required 
+								id="Validade" 
+								label="Validade" 
+								variant="outlined"
+								fullWidth />
+						</Grid>
+						<Grid item xs={12} md={6}>
+							<TextField
+								required
+								id="cvv"
+								label="CVV"
+								helperText="Três números no verso do cartão"
+								variant="outlined"
+								fullWidth
+							/>
+						</Grid>
+						<Grid item xs={12}>
+							<FormControlLabel
+								control={<Checkbox color="secondary" name="saveCard" value="yes" />}
+								label="Salvar dados do cartão para próximas compras"
+							/>
+						</Grid>
+					</Grid>
+				</React.Fragment>
+			</Grid>
+
+			<Grid item xs={12}>
+				<Button
+					variant="contained"
+					color="primary"
+				>
+					Prosseguir
+				</Button>
+			</Grid>
+
+		</Grid>
+
+	</div>
+
 );
 
 export default Formulario;
--- src/modules/checkout/resumo.js
+++ src/modules/checkout/resumo.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
 
 const Resumo = () => (
-  <section className='resumo'>
-    Resumo
-  </section>
+
+	<Grid container spacing={24} className="info-item">
+		<Grid item xs={12}>
+			<h2>Instituição</h2>
+			<h3>OHC Toronto - Semi-intensivo</h3>
+			<p>Na OHC, achamos que aprender e estudar inglês deve ser fácil, divertido e recompensador! Nossos alunos parecem concordar conosco desde que começamos a ensinar em 1974 e têm crescido desde então!</p>
+			<span className='item-amount'>R$ 6.000,00</span>
+		</Grid>
+
+		<Grid item xs={12} className="info-item">
+			<h2>Anfitrião</h2>
+			<h3>Família Adams</h3>
+			<p>[Casa] Alexander Street, N°: 666</p>
+			<p>Tipo de acomodação: Casa</p>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took.</p>
+			<span className='item-amount'>R$ 2.259,00</span>
+		</Grid>
+
+		<Grid item xs={12} className="info-item">
+			<h2>Dados do Vôo</h2>
+			<h3>América Airlines</h3>
+			<p>Checkin: 02/07/2019 16h30</p>
+			<p>Checkout: 30/07/2019 13h10</p>
+			<span className='item-amount'>R$ 4.000,00</span>
+		</Grid>
+
+		<Grid item xs={12} className='amount info-item'>
+			R$ 12.259,00
+		</Grid>
+
+	</Grid>
+
 );
 
 export default Resumo;
