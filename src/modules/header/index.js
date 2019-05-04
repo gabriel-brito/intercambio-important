96e1a81cc7e49185e1972e82592e7a9939026162
ubirajarapelli <ubirajaracpelli@gmail.com>
2019-05-04 02:36:40 -0300

    Add Header, chips, step one, step two e imagens

---
+++ src/modules/header/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+const Header = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <IconButton color="inherit" aria-label="Menu">
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" color="inherit" style={styles.grow}>
+                    !Important
+                </Typography>
+                <IconButton color="inherit">
+                  <AccountCircle />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
+        );
+    }
+
+export default withStyles(styles)(Header)
